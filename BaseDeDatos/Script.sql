1a4eff4b77e937eb4ba4b596a6073eba2932a6b4
Delia Montoya Casas <delia.montoya867@gmail.com>
2025-03-19 06:56:02 -0600

    Updating files

---
+++ BaseDeDatos/Script.sql
@@ -0,0 +1,127 @@
+drop table sitios;
+
+create table sitios(
+	gmap_id varchar(50) primary key,
+	category varchar(20),
+	name varchar(20),
+	address varchar(100),
+	descripcion varchar(100) ,
+	latitude varchar(20),
+	longitude varchar(20),
+	avg_rating float,
+	num_of_reviews numeric,
+	price float,
+	hours json,
+	MISC json,
+	state varchar(25),
+	relative_results json,
+	URL json
+);
+
+select * from sitios;
+
+create table business(
+	business_id varchar(20) primary key,
+	name varchar(20),
+	address varchar(20),
+	city varchar(20),
+	state varchar(25),
+	postal_code varchar(10),
+	latitude varchar(20),
+	longitude varchar(20),
+	starts float,
+	review_count smallint,
+	is_open smallint,
+	object json,
+	categories json,
+	hour json
+);
+
+select * from business;
+
+create table user_yelp(
+	user_id varchar(30) primary key,
+	name varchar(20),
+	review_count smallint,
+	yelping_since date,
+	friends json,
+	useful smallint,
+	funny smallint,
+	cool smallint,
+	fans smallint,
+	elite json,
+	average_starts float,
+	compliment_hot smallint,
+	compliment_more smallint,
+	compliment_profile smallint,
+	compliment_cute smallint,
+	compliment_list smallint,
+	compliment_note smallint,
+	compliment_plain smallint,
+	compliment_cool smallint,
+	compliment_funny smallint,
+	compliment_writer smallint,
+	compliment_photos smallint
+);
+
+select * from user_yelp;
+
+drop table estados;
+create table estados(
+	estados_id serial primary key,
+	user_id varchar(30),
+	gmap_id varchar(50),
+	name varchar(60),
+	time  text,
+	rating smallint,
+	text text,
+	pics json,
+	resp json,
+	FOREIGN KEY (user_id) REFERENCES user_yelp,
+	FOREIGN KEY (gmap_id) REFERENCES sitios
+);
+
+select * from estados;
+
+
+
+create table review(
+	review_id numeric primary key,
+	starts smallint,
+	date_review date,
+	text_review text,
+	useful smallint,
+	funny smallint,
+	cool smallint,
+	user_id varchar(30),
+	business_id varchar(20),
+	FOREIGN KEY (user_id) REFERENCES user_yelp,
+	FOREIGN KEY (business_id) REFERENCES business	
+);
+
+select * from review;
+
+drop table checkin;
+create table checkin(
+	checkin_id serial primary key,
+	date_checkin date[],
+	business_id varchar(20),
+	FOREIGN KEY (business_id) REFERENCES business	
+);
+
+select * from checkin;
+
+drop table tip;
+create table tip(
+	tip_id serial primary key,
+	text_tip text,
+	date_tio date,
+	compliment_count smallint,
+	user_id varchar(30),
+	business_id varchar(20),
+	FOREIGN KEY (user_id) REFERENCES user_yelp,
+	FOREIGN KEY (business_id) REFERENCES business	
+);
+
+select * from tip;
+
--- Readme.md
+++ Readme.md
@@ -2,18 +2,33 @@ Version de Postgres:
 ![alt text](image.png)
 
 Tareas a Realizar
--   Definir Que base de datos a Utilizar
--   Instalación Base de Datos: Propuesta PostgreSQL
--   Creación del Diagrama De Base de Datos
-    -   Crear tablas
-    -   Indices
-    -   Store Procedures (Si es que es necesario)
-    -   Funciones (Si es que es necesario)
-    -   Triggers (Si es que es necesario)
--   Crear notebook para la limpieza  de los archivos de tipo JSON, PKL, parquet, etc
--   Subir los archivos a la nube GCP
-    -   Definir la organización de la carpeta que contiene los archivos (en zip?)
-    -   Planificar la sincronización con el bucket de GSC (si es necesario)
+-   Base de Datos
+    -   Definir Que base de datos a Utilizar
+    -   Instalación Base de Datos: Propuesta PostgreSQL
+
+-   DataLake    
+        -   Definir la organización de la carpeta que contiene los archivos (en zip?)
+        -   Planificar la sincronización con el bucket de GSC (si es necesario)
+
+-   Diseño Base de Datos  --> Delia
+    -   Creación del Diagrama
+        -   Crear tablas
+        -   Indices
+
+-   API --> Alejandro
+    -   Definir API a utilizar
+    -   Definir Integración
+    -   Obtención de datos
+    -   Guardar Datos
     
+
+-   EDA/ETL
+    -   Crear notebook para la limpieza  de los archivos de tipo JSON, PKL, parquet, etc y Filtrado --> Dependemos de Ariel
+    -   Cargar los Datos A Base de Datos    --> Dependemos del punto anterior
+    
+
+
+
+
 
 
