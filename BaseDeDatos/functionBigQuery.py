bb34910c59a92afd5847e626b12a0cbc5e70890b
Delia Montoya Casas <delia.montoya867@gmail.com>
2025-04-09 10:22:21 -0600

    updating files ETL

--- BaseDeDatos/functionBigQuery.py
+++ BaseDeDatos/functionBigQuery.py
@@ -83,26 +83,28 @@ def read_json_content(content):
     return json_file
 
 def etl_review_json_file(file_path):
-    # This is how you read a BigQuery table
+    json_file = []
+    #   This is how you read a BigQuery table
     client = storage.Client.from_service_account_json('adsac-455509-0d5538a0d624.json')
     bucket = client.get_bucket('adsac')
-    #blob = bucket.blob('Yelp/review.json')
     blob = bucket.blob(file_path)
-    content = blob.download_as_text()
+    # Stream and process the blob in chunks
+    with blob.open("r", encoding="utf-8") as file_obj:
+        for line in file_obj:
+            try:
+                json_line = json.loads(line.strip())
+                json_file.append(json_line)
+            except json.JSONDecodeError:
+                print("Invalid JSON line:", line.strip())
     
-    json_file = read_json_content(content)
-
-    df_sitios = pd.DataFrame(json_file)
-    df_sitios_no_duplicates = df_sitios.drop_duplicates(['name','gmap_id','latitude','longitude','address'])
-
-    df_sitios_no_duplicates = df_sitios_no_duplicates.drop(columns=['price','relative_results','url'])
-    
-    #Insertar en la tabla curada
+    df_review = pd.DataFrame(json_file)
+    df_review = df_review.drop_duplicates(['review_id', 'user_id', 'business_id', 'stars', 'useful', 'funny', 'cool', 'text'])
     destination_table = 'adsac-455509.Curated.Review'
 
     # Upload DataFrame
-    to_gbq(df_sitios_no_duplicates, destination_table, project_id=PROJECT_ID, if_exists='replace')  # Use 'append' if you want to add rows
+    to_gbq(df_review, destination_table, project_id=PROJECT_ID, if_exists='replace')  # Use 'append' if you want to add rows
     print("Termino con éxito la creación de la tabla:",destination_table)
+
 
 def etl_checkin_json_file(file_path):
     # This is how you read a BigQuery table
@@ -149,10 +151,86 @@ def etl_business_json_file(file_path):
     to_gbq(df_business, destination_table, project_id=PROJECT_ID, if_exists='replace')  # Use 'append' if you want to add rows
     print("Termino con éxito la creación de la tabla:",destination_table)
 
+def etl_tip_json_file(file_path):
+    # This is how you read a BigQuery table
+    client = storage.Client.from_service_account_json('adsac-455509-0d5538a0d624.json')
+    bucket = client.get_bucket('adsac')
+    #blob = bucket.blob('Yelp/tip.json')
+    blob = bucket.blob(file_path)
+    content = blob.download_as_text()
+    
+    json_file = read_json_content(content)
+
+    df_tip = pd.DataFrame(json_file)
+
+    # Removing rows where state is None
+    df_tip = df_tip.dropna(subset=['user_id'])
+
+    # Removing rows where city is None
+    df_tip = df_tip.dropna(subset=['business_id'])
+
+    df_tip = df_tip.drop_duplicates()
+    #Insertar en la tabla curada
+    destination_table = 'adsac-455509.Curated.Tip'
+    # Upload DataFrame
+    to_gbq(df_tip, destination_table, project_id=PROJECT_ID, if_exists='replace')  # Use 'append' if you want to add rows
+    print("Termino con éxito la creación de la tabla:",destination_table)
+
+def identify_restaurant(ref):
+    #print(ref)
+    if not ref:
+        return 0
+    value =  [item for item in ref if "rest" in item]
+    
+    if not value:
+        return 0
+    return 1
+
+def etl_sitios_json_file(file_path):
+    client = storage.Client.from_service_account_json('adsac-455509-0d5538a0d624.json')
+    bucket = client.get_bucket('adsac')
+    #blob = bucket.blob('Google/1.json')
+    blob = bucket.blob(file_path)
+
+    json_file = []
+    with blob.open("r", encoding="utf-8") as file_obj:
+        for line in file_obj:
+            try:
+                json_line = json.loads(line.strip())
+                json_file.append(json_line)
+            except json.JSONDecodeError:
+                print("Invalid JSON line:", line.strip())
+
+    df_sitios = pd.DataFrame(json_file)
+    
+    f_sitios = df_sitios.drop_duplicates(['name','gmap_id','latitude','longitude','address'])
+    df_sitios = df_sitios.drop(columns=['price'])
+    df_sitios['is_restaurant'] = df_sitios.category.apply(identify_restaurant)
+    
+
+
+    df_sitios['category'] = df_sitios['category'].astype(str)
+    df_sitios['hours'] = df_sitios['hours'].astype(str)
+    df_sitios['MISC'] = df_sitios['MISC'].astype(str)
+    df_sitios['relative_results'] = df_sitios['relative_results'].astype(str)
+
+
+    df_sitios = df_sitios[df_sitios['category'].str.contains('resta', case=False, na=False)]
+
+    df_sitios = df_sitios[df_sitios["is_restaurant"] == 1]
+
+    #Insertar en la tabla curada
+    destination_table = 'adsac-455509.Curated.Sitios'
+    # Upload DataFrame
+    to_gbq(df_sitios, destination_table, project_id=PROJECT_ID, if_exists='replace')  # Use 'append' if you want to add rows
+    print("Termino con éxito la creación de la tabla:",destination_table)
+
 def etl():
     #etl_picklet_file('gs://adsac/Yelp/business.pkl')
     #etl_parquet_file('gs://adsac/Yelp/user.parquet')
     #etl_checkin_json_file('Yelp/checkin.json')
-    etl_review_json_file('Yelp/review.json')
+    #etl_review_json_file('Yelp/review.json') volver a ejecutar solo para validar por el hard coded q estaba
+    #etl_tip_json_file('Yelp/tip.json')
+    #etl_sitios_json_file('Google/1.json')
    
     
